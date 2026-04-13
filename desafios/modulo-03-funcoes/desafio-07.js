8b978aed1e9020a86dfe4b6919bb7717d8e6aee4
Lucas Natanael <naillathdev@gmail.com>
2026-04-13 09:47:52 -0300

    feat: adiciona resolução do desafio-07 do modulo-03

--- desafios/modulo-03-funcoes/desafio-07.js
+++ desafios/modulo-03-funcoes/desafio-07.js
@@ -10,3 +10,37 @@
 
 // Simular decisão de jogo
 // Trabalhar lógica condicional mais narrativa
+
+function mostrarCaminhos() {
+    const caminhos = ["[1] - Esquerda", "[2] - Direita"];
+
+    for (const caminho of caminhos) {
+        console.log(caminho);
+    }
+}
+
+function caminhoEscolhido(caminho) {
+    if (typeof caminho !== "string" || caminho.trim() === "") {
+        return "\nCaminho desconhecido até mesmo pelos deuses..."
+    }
+
+    caminho = caminho.toLowerCase();
+
+    if (caminho !== "esquerda" && caminho !== "direita") {
+        return "\nCaminho inválido, jovem aventureiro!"
+    }
+
+    if (caminho === "esquerda") {
+        return "\nVocê encontrou uma tropa de Orc Guerreiros!";
+    } else {
+        return "\nVocê encontrou um tesouro!";
+    }
+}
+
+mostrarCaminhos();
+console.log(caminhoEscolhido("cima"));
+console.log(caminhoEscolhido());
+console.log(caminhoEscolhido(123));
+console.log(caminhoEscolhido("cima"));
+console.log(caminhoEscolhido("esquerda"));
+console.log(caminhoEscolhido("direita"));
